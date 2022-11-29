551ca949277ba5cf6f28e78dd8535cf9244e397b
Andres Ramirez <andres200493@gmail.com>
2022-11-28 21:41:21 -0600

    S2 - R2

--- s2/e2/src/schema.js
+++ s2/e2/src/schema.js
@@ -3,13 +3,21 @@ const { buildSchema } = require('graphql')
 const schema = buildSchema(`
   type Query {
     saludo: String,
-    experto(id:ID!): Experto,
-    getAllExpertos: [Experto]
+    getExperto(id:ID!): Experto,
+    getAllExpertos: [Experto],
+    getBook(asin:ID!): Book,
+    getAllBooks: [Book]
   },
   type Experto {
     id: ID,
     nombre: String,
     apellido: String
+  },
+  type Book {
+    asin: ID,
+    title: String,
+    author: String,
+    pages: Int
   }
 `)
 
