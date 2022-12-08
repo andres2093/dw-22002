7c8ba15c8b9938321de1850a89d76795790deb45
Andres Ramirez <andres200493@gmail.com>
2022-12-07 21:56:09 -0600

    S5 - R2, R3

--- s5/e1/src/schema.js
+++ s5/e1/src/schema.js
@@ -2,7 +2,12 @@ import { gql } from 'apollo-server'
 
 export const typeDefs = gql`
   type Query {
-    getAllBooks: [Book]
+    getAllBooks: [Book],
+    getBook(asin:ID!): Book
+  }
+  type Mutation {
+    insertBook(asin:ID!,title:String, author:String, pages:Int): Book,
+    updateBook(asin:ID!,title:String, author:String, pages:Int): Book
   }
   type Book {
     asin: ID,
