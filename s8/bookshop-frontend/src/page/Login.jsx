a961bc00cd2b8df8c4ec3cf6193aed889812d083
Andres Ramirez <andres200493@gmail.com>
2023-01-09 20:00:41 -0600

    S8 - R1

--- s8/bookshop-frontend/src/page/Login.jsx
+++ s8/bookshop-frontend/src/page/Login.jsx
@@ -1,7 +1,12 @@
 export const Login = () => {
   return (
-      <section className="title">
-          Login page
-      </section>
+      <div>
+        <form onSubmit="">
+          <h3>Login</h3>
+          <input type="address" placeholder="Email"/>
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
   )
 }
--- s8/bookshop-frontend/src/page/Register.jsx
+++ s8/bookshop-frontend/src/page/Register.jsx
@@ -1,7 +1,14 @@
 export const Register = () => {
   return (
-      <section className="title">
-          Register page
-      </section>
+    <div>
+      <form onSubmit="">
+        <h3>Register</h3>
+        <input type="text" placeholder="Name"/>
+        <input type="lastname" placeholder="Lastname"/>
+        <input type="address" placeholder="Email"/>
+        <input type="password" placeholder="Password" />
+        <button type="submit">Register</button>
+      </form>
+    </div>
   )
 }
