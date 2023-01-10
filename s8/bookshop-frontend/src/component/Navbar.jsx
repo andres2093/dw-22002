2ded8ec0f684f40cacf674d0dcbc9862d920feb1
Andres Ramirez <andres200493@gmail.com>
2023-01-09 20:17:03 -0600

    S8 - E2

--- s8/bookshop-frontend/src/component/Navbar.jsx
+++ s8/bookshop-frontend/src/component/Navbar.jsx
@@ -1,6 +1,19 @@
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { AuthService } from '../auth'
 
 export const Navbar = () => {
+
+  const [signedUser, setSignedUser] = useState(false);
+
+  useEffect(() => {
+    setSignedUser(AuthService.isUserAuthenticated())
+  }, []);
+
+  const logOut = () => {
+    AuthService.logout();
+  };
+
   return (
     <nav className="navbar navbar-expand navbar-dark bg-primary">
       {/* Elementos a la izquierda */}
@@ -10,19 +23,37 @@ export const Navbar = () => {
             Home
           </Link>
         </li>
+        {signedUser && (
+          <li className="nav-item">
+            <Link to={"/books"} className="nav-link">
+              Books
+            </Link>
+          </li>
+        )}
       </div>
+
       {/* Elementos a la derecha */}
       <div className="navbar-nav ms-auto">
-        <li className="nav-item">
-          <Link to={"/login"} className="nav-link">
-            Login
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to={"/register"} className="nav-link">
-            Register
-          </Link>
-        </li>
+        {signedUser ? (
+          <li className="nav-item">
+            <a href="/signin" className="nav-link" onClick={logOut}>
+              Logout
+            </a>
+          </li>
+        ) : (
+          <>
+            <li className="nav-item">
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/register"} className="nav-link">
+                Register
+              </Link>
+            </li>
+          </>
+        )}
       </div>
     </nav>
   )
